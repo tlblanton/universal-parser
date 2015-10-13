db0174fcb79bf3985853472fbb13850d314b54f8
Tyler Blanton <tylerblanton635@gmail.com>
2015-10-13 15:04:21 -0600

    created scanner.h and scanner.cpp

--- scanner.cpp
+++ scanner.cpp
@@ -1,1 +1,9 @@
 
+#include "scanner.h"
+#include <iostream>
+#include "sets.h"
+#include "grammarAnalyzer.h"
+#include "LLDriver.h"
+
+using namespace std;
+
--- scanner.h
+++ scanner.h
@@ -1,1 +1,15 @@
 
+#ifndef _SCANNER_H
+#define _SCANNER_H
+
+#include <iostream>
+#include "sets.h"
+#include "grammarAnalyzer.h"
+#include "LLDriver.h"
+
+using namespace std;
+
+
+
+
+#endif
