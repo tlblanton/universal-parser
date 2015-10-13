65b40adb280b0af49ea884fd11c4d8b7bef9ef25
Tyler Blanton <tylerblanton635@gmail.com>
2015-10-13 13:56:54 -0600

    Works, but doesn't have scanner, and syntax errors aren't handled correctly.

--- LLDriver.cpp
+++ LLDriver.cpp
@@ -14,7 +14,7 @@ extern MarkedVocabulary myVocab;
 extern std::vector< std::vector<string> > tableVect;
 
 
-void LLDrriver()
+void LLDriver()
 {
 	string currentThing;
 	currentThing = "TEST";//scanner(); //need to implemet scanner to read program. use other one?
@@ -25,14 +25,34 @@ void LLDrriver()
 		{
 				if(T(myStack.top(), currentThing) != -1)
 				{
-
+					int index = findIndex(g.LHS, myStack.top());
+					std::vector<string> NTProductions;
+					NTProductions = getProductions(g.RHS[index]);
+					for(int i = NTProductions.size()-1; i > -1; --i)
+					{
+						cout << "putting " << NTProductions[i] << " onto the stack " << endl;
+						myStack.push(NTProductions[i]);
+					}
 				}
 				else
 				{
 					cout << "syntax error on " << currentThing << endl;
+					//need to advance currentThing I think. Need to do something.
 				}
 		}
+		else //means X is in terminals
+		{
+			cout << "in else\n";
+			if(myStack.top() == currentThing)
+			{
+				myStack.pop();
+				currentThing = "NEXT";//scanner(); //getting next token form user program
+			}
+			else
+			{
+				cout << "syntax error on " << currentThing << endl;
+				//need way to advance here, just like above
+			}
+		}
 	}
-
-
 }
